9c0b333c1f98eb62338b4f1045d39ecd58ca13da
Vasily Ostanin <bazilio91@gmail.com>
2016-10-20 21:55:11 +0500

    move consul to bg

--- consul.sh
+++ consul.sh
@@ -8,6 +8,6 @@ cp /etc/consul/consul.json.tpl /etc/consul/consul.json
     -dc $CONSUL_DC \
     -domain $CONSUL_DOMAIN \
     -config-file /etc/consul/consul.json \
-    -config-dir /etc/consul/conf.d
+    -config-dir /etc/consul/conf.d &
 
 source /entrypoint.sh
